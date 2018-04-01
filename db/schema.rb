d4427787ce96eac3ff873d1e7eaf33ac03c00023
hungkieu <hungkieu.h12@gmail.com>
2018-04-01 10:55:04 +0700

    chap-12:addpassword_reset

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,5 @@
-ActiveRecord::Schema.define(version: 20180330091231) do
+ActiveRecord::Schema.define(version: 20180401030133) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -10,6 +11,9 @@ ActiveRecord::Schema.define(version: 20180330091231) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
